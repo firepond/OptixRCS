6412fd6ae9381833576a1abb2611acc9e74828ea
firepond <915644848@qq.com>
2023-08-05 16:56:02 +0100

    Add operator reload.

--- RcsSpeedBranch/reduceKernels.cu
+++ RcsSpeedBranch/reduceKernels.cu
@@ -1,24 +1,39 @@
-//#include <cuda/helpers.h>
+// #include <cuda/helpers.h>
 #include <cuda_runtime.h>
-#include "reduceKernels.h"
-#include "rcs_params.h"
 #include <sutil/vec_math.h>
+
 #include <cmath>
+
+#include "rcs_params.h"
+#include "reduceKernels.h"
 
 __constant__ Result zero;
 
 
-__global__ void reduceKernel(Result* g_idata, Result* g_odata, int size) {
 
+__device__ Result operator+(const Result& a, const Result& b){
+	Result res;
+
+	res.ar_real = a.ar_real + b.ar_real;
+	res.ar_img = a.ar_img + b.ar_img;
+
+	res.au_real = a.au_real + b.au_real;
+	res.au_img = a.au_img + b.au_img;
+	return res;
+}
+
+
+__global__ void reduceKernel(Result* g_idata, Result* g_odata, int size) {
 	int temp = (blockIdx.x + 1) * blockDim.x;
 
-	int curBlockSize = (temp <= size) * blockDim.x + (temp > size) * (size % blockDim.x);
+	
 	extern __shared__ Result sdata[512];
 
 	// each thread loads one element from global to shared mem
 	unsigned int tid = threadIdx.x;
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (tid < curBlockSize) {
+
+	if (i < size) {
 		sdata[tid] = g_idata[i];
 	}
 	else {
@@ -30,11 +45,7 @@ __global__ void reduceKernel(Result* g_idata, Result* g_odata, int size) {
 
 	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
 		if (tid < s) {
-			sdata[tid].ar_img += sdata[tid + s].ar_img;
-			sdata[tid].ar_real += sdata[tid + s].ar_real;
-			sdata[tid].au_img += sdata[tid + s].au_img;
-			sdata[tid].au_real += sdata[tid + s].au_real;
-	
+			sdata[tid] = sdata[tid] + sdata[tid + s];
 		}
 		__syncthreads();
 	}
@@ -43,12 +54,9 @@ __global__ void reduceKernel(Result* g_idata, Result* g_odata, int size) {
 	if (tid == 0) {
 		g_odata[blockIdx.x] = sdata[0];
 	}
-
 }
 
-
-Result reduce(Result* g_idata, int size)
-{
+Result reduce(Result* g_idata, int size) {
 	int blockDim = 512;
 
 	int reduceCount = ceil(log2(size) / log2(blockDim));
@@ -62,14 +70,15 @@ Result reduce(Result* g_idata, int size)
 
 	while (size > 1) {
 		if (size <= blockDim) {
-			reduceKernel <<< 1, blockDim >>> (to_reduce_device, out_device, size);
+			reduceKernel << <1, blockDim >> > (to_reduce_device, out_device, size);
 			break;
 		}
 		else {
 			block_count = ceil((double)size / blockDim);
-			reduceKernel <<< block_count, blockDim >>> (to_reduce_device, out_device, size);
+			reduceKernel << <block_count, blockDim >> > (to_reduce_device,
+				out_device, size);
 		}
-	
+
 		cudaDeviceSynchronize();
 
 		// swap to_reduce_device and out_device
@@ -82,10 +91,8 @@ Result reduce(Result* g_idata, int size)
 	}
 
 	Result result_out;
-	cudaMemcpy(&result_out, out_device, sizeof(Result),
-		cudaMemcpyDeviceToHost);
+	cudaMemcpy(&result_out, out_device, sizeof(Result), cudaMemcpyDeviceToHost);
 	cudaFree(reinterpret_cast<void*>(out_device_holder));
 	cudaDeviceSynchronize();
 	return result_out;
 }
-
